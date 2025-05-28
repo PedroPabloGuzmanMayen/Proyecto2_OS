90f95100488610c9fa68a15ba4311879bfc8f746
EstebanZG999 <zam22119@uvg.edu.gt>
2025-05-28 13:57:47 -0600

    feat: added priority scheduling, SJF

--- include/algoritmo.h
+++ include/algoritmo.h
@@ -14,4 +14,9 @@ std::vector<Proceso> fifo(const std::vector<Proceso>& procesos);
 std::vector<Proceso> roundRobin(const std::vector<Proceso>& procesos, int quantum, std::vector<BloqueGantt>& bloques);
 double calcularTiempoEsperaPromedio(const std::vector<Proceso>& procesosOriginal, const std::vector<Proceso>& ejecucion);
 
+//Shortest Job First
+std::vector<Proceso> shortestJobFirst(const std::vector<Proceso>& procesos);
+//Priority Scheduling
+std::vector<Proceso> priorityScheduling(const std::vector<Proceso>& procesos);
+
 #endif
